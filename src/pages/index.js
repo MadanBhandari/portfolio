52f8eb2a38e94ea37de0200f6f78ac1be8b250ab
Madan Bhandari <madan.kbh@gmail.com>
2018-09-25 15:23:09 +0530

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Landing from '../components/landing'
 
 const IndexPage = () => (
   <Landing>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
   </Landing>
 )
 
