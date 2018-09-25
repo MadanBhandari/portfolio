8094a43ff5b9e8b85146b36654766a4045bf0f66
Madan Bhandari <madan.kbh@gmail.com>
2018-09-25 16:11:41 +0530

    header content updated

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,24 +1,36 @@
 import React from "react";
 import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
 export default function BlogPage({ data }) {
   const { edges: posts } = data.allMarkdownRemark;
   return (
-    <div className="blog-posts">
-      {posts
-        .filter(post => post.node.frontmatter.title.length > 0)
-        .map(({ node: post }) => {
-          return (
-            <div className="blog-post-preview" key={post.id}>
-              <h1>
-                <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
-              </h1>
-              <h2>{post.frontmatter.date}</h2>
-              <p>{post.excerpt}</p>
-            </div>
-          );
-        })}
-    </div>
+    <>
+      <Helmet
+        title='Blogs - Madan Bhandari'
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      >
+        <html lang="en" />
+      </Helmet>
+      <div className="blog-posts">
+        {posts
+          .filter(post => post.node.frontmatter.title.length > 0)
+          .map(({ node: post }) => {
+            return (
+              <div className="blog-post-preview" key={post.id}>
+                <h1>
+                  <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+                </h1>
+                <h2>{post.frontmatter.date}</h2>
+                <p>{post.excerpt}</p>
+              </div>
+            );
+          })}
+      </div>
+    </>
   );
 }
 export const pageQuery = graphql`
