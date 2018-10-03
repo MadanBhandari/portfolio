acea8df112a4d429ea18a4f19db12872a11abca2
Madan Bhandari <madan.kbh@gmail.com>
2018-10-03 11:43:29 +0530

    Home page updates

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,43 @@
 import React from 'react'
 import Layout from '../components/layout'
+import styled from 'styled-components'
+import Helmet from 'react-helmet'
+
+const HomeMain = styled.div`
+  position: relative;
+  padding: 1em;
+  h3{
+    font-size: 2em;
+  }
+`
+const IntroContent = styled.div`
+  position: relative;
+  font-size: 1.5em;
+  max-width: 600px;
+  line-height: 1.5em;
+  font-weight: 300;
+  span{
+    font-weight: 600;
+  }
+`
 
 const HomePage = () => (
   <Layout>
-    <h1>Home Page</h1>
-    <p>This will be the new home. - WIP</p>
+    <Helmet
+      title='Madan Bhandari - Fullstack Developer'
+      meta={[
+        { name: 'description', content: 'Madan Bhandari Portfolio' },
+        { name: 'keywords', content: 'fullstack, developer, portfolio' },
+      ]}
+    >
+      <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet"></link>
+    </Helmet>
+    <HomeMain>
+      <h3>Hello!</h3>
+      <IntroContent>
+        I'm <span>Madan Bhandari</span>, a frontend minded fullstack software engineer building web a better place.
+      </IntroContent>
+    </HomeMain>
   </Layout>
 )
 
